7d209eed9c504b6c85d72c43319260ba58cfeafc
Luka Stärk <luka.staerk@outlook.com>
2023-06-01 16:22:16 +0200

    update

--- config.py
+++ config.py
@@ -1,3 +1,12 @@
+from envbash import load_envbash
+import os
+import shutil
+if not os.path.exists("config.env"):
+    # copy "scripts/config.env.default" to "config.env" with shutil.copyfile(src, dst)
+    shutil.copyfile("scripts/config.env.default", "config.env")
+
+load_envbash("config.env")
+
 BLOCK1, BLOCK2 = "block1", "block2"
 BLOCK = BLOCK1
 
@@ -13,4 +22,4 @@ BATCH="batch"
 DATAFRAME="DATAFRAME"
 HOURS_PER_DAY = 8
 FRAMES_PER_SECOND = 5
-projectPath = "/Volumes/Extreme_SSD/content/Fish_moves_new"
+projectPath = os.environ["projectPath"]

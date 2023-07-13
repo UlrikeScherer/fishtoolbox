86cc776976e86040d283ad25264bac5771418db0
bmarvin <beesemarvin@gmail.com>
2023-07-13 19:36:32 +0200

    building and unifying entropy tables implemented

--- data_factory/table_export.py
+++ data_factory/table_export.py
@@ -98,6 +98,209 @@ def get_melted_table(df):
     result["minutes"] = result[DATAFRAMES] // (60*5)
     return result.dropna()
 
+def load_entropy_data(
+    parameters, 
+    time_range = ['daily', 'hourly'],
+    cluster_sizes = ['005', '007', '010', '020', '050']
+):
+    entropy_data_dict = {}
+    for time in time_range:
+        for size in cluster_sizes:
+            file_name = parameters.projectPath+f'/plasticity/cluster_entropy_wshed/cluster_entropy_wshed_{time}_{size}.csv'
+            entropy_data_dict[f'{time}_{size}'] = pd.read_csv(file_name)
+    return entropy_data_dict
+
+
+def load_coefficient_of_variation_data(
+    parameters,
+    time_range = ['daily', 'hourly'],
+    accuracies = ['050'],
+    principal_components = ['distance to wall', 'turning angle', 'step length']
+):
+    cv_data_dict = {}
+    for time in time_range:
+        for accuracy in accuracies:
+            for mode in principal_components:
+                file_name = parameters.projectPath+f'/plasticity/cv/cv_{mode}_{time}_ndf_{accuracy}.csv'
+                if mode == 'distance to wall':
+                    mode_shrtnd = 'd2w'
+                elif mode == 'turning angle':
+                    mode_shrtnd = 'angle'
+                elif mode == 'step length':
+                    mode_shrtnd = 'step'
+                cv_data_dict[f'{time}_{accuracy}_{mode_shrtnd}'] = pd.read_csv(file_name)
+    return cv_data_dict
+
+
+def build_singular_ids_table(
+    input_data_dict,
+    fish_keys,
+    time_constraint,
+    measure = 'entropy',
+    modes = ['005', '007', '010', '020', '050'],
+    cov_accuracy = '050',
+):
+    output_id_dict = {}
+    for id in fish_keys:
+        # cv values
+        intermediate_dict = {}
+        for mode in modes:
+            if (measure == 'coefficient of variation') or (measure == 'cov'):
+                identifier_str = f'{time_constraint}_{cov_accuracy}_{mode}'
+                column_identifier_str = f'{mode}'
+            elif measure == 'entropy':
+                identifier_str = f'{time_constraint}_{mode}'
+                column_identifier_str = f'entropy_{mode}'
+            mode_df = input_data_dict[f'{identifier_str}'][[id, 'Unnamed: 0']]
+            mode_df = mode_df.rename(columns={'Unnamed: 0': 'timestep', f'{id}': f'{column_identifier_str}'})
+            mode_df['id'] = id
+            intermediate_dict[f'{mode}'] = mode_df
+        output_id_dict[f'{id}'] = intermediate_dict
+    return output_id_dict
+
+
+def merge_cov_and_entropy_dicts_to_one_df(
+    cov_dict,
+    entropy_dict,
+    fish_keys,
+    time_constraint = 'daily',
+    cov_modes = ['d2w', 'angle', 'step'],
+    cov_accuracy = '050',
+    entropy_modes = ['005', '007', '010', '020', '050'],
+    output_file_name = None
+):
+    
+    cv_dfs_list = []
+    entropy_dfs_list = []
+    
+    for id in fish_keys:
+        # cov values
+        cv_dict = cov_dict[id]
+        cv_df = pd.merge(
+            pd.merge(
+                cv_dict[cov_modes[0]], cv_dict[cov_modes[1]], 
+                on=['timestep', 'id']
+            ), cv_dict[cov_modes[2]],
+            on=['timestep', 'id']
+        )
+        cv_dfs_list.append(cv_df)
+        
+        # entropy values
+        entropy_specified_dict = entropy_dict[id]
+        if len(entropy_modes) == 5:
+            entropy_df = pd.merge(
+                pd.merge(
+                    pd.merge(
+                        pd.merge(
+                            entropy_specified_dict[entropy_modes[0]], entropy_specified_dict[entropy_modes[1]],
+                            on=['timestep', 'id']
+                        ), entropy_specified_dict[entropy_modes[2]],
+                        on=['timestep', 'id']
+                    ), entropy_specified_dict[entropy_modes[3]],
+                    on=['timestep', 'id']
+                ), entropy_specified_dict[entropy_modes[4]],
+                on=['timestep', 'id']
+            )
+        elif len(entropy_modes) == 4:
+            entropy_df = pd.merge(
+                pd.merge(
+                    pd.merge(
+                        entropy_specified_dict[entropy_modes[0]], entropy_specified_dict[entropy_modes[1]],
+                        on=['timestep', 'id']
+                    ), entropy_specified_dict[entropy_modes[2]],
+                    on=['timestep', 'id']
+                ), entropy_specified_dict[entropy_modes[3]],
+                on=['timestep', 'id']
+            )
+        elif len(entropy_modes) == 3:
+            entropy_df = pd.merge(
+                pd.merge(
+                    entropy_specified_dict[entropy_modes[0]], entropy_specified_dict[entropy_modes[1]],
+                    on=['timestep', 'id']
+                ), entropy_specified_dict[entropy_modes[3]],
+                on=['timestep', 'id']
+            )
+        elif len(entropy_modes) == 2:
+            entropy_df = pd.merge(
+                entropy_specified_dict[entropy_modes[0]], entropy_specified_dict[entropy_modes[1]],
+                on=['timestep', 'id']
+            )
+        entropy_dfs_list.append(entropy_df)
+
+    cv_all_df = pd.concat(cv_dfs_list)
+    entropy_all_df = pd.concat(entropy_dfs_list)
+
+    cols = ['timestep', 'id']
+    all_df = pd.merge(cv_all_df, entropy_all_df, on=cols)
+    if len(entropy_modes) == 5:
+        all_df_reordered = all_df[['timestep', 'id', cov_modes[0], cov_modes[1], cov_modes[2], f'entropy_{entropy_modes[0]}', f'entropy_{entropy_modes[1]}', f'entropy_{entropy_modes[2]}', f'entropy_{entropy_modes[3]}', f'entropy_{entropy_modes[4]}']]
+    elif len(entropy_modes) == 4:
+        all_df_reordered = all_df[['timestep', 'id', cov_modes[0], cov_modes[1], cov_modes[2], f'entropy_{entropy_modes[0]}', f'entropy_{entropy_modes[1]}', f'entropy_{entropy_modes[2]}', f'entropy_{entropy_modes[3]}']]
+    elif len(entropy_modes) == 3:
+        all_df_reordered = all_df[['timestep', 'id', cov_modes[0], cov_modes[1], cov_modes[2], f'entropy_{entropy_modes[0]}', f'entropy_{entropy_modes[1]}', f'entropy_{entropy_modes[2]}']]
+    elif len(entropy_modes) == 2:
+        all_df_reordered = all_df[['timestep', 'id', cov_modes[0], cov_modes[1], cov_modes[2], f'entropy_{entropy_modes[0]}', f'entropy_{entropy_modes[1]}']]
+    if output_file_name is not None: 
+        all_df_reordered.to_csv(output_file_name)
+    return all_df_reordered
+
+
+def build_and_unify_cov_and_entropy_tables(
+    parameters,
+    fish_keys,
+    time_range_extraction= ['daily', 'hourly'],
+    time_constraint = 'daily',
+    cov_principal_components = ['distance to wall', 'turning angle', 'step length'],
+    cov_accuracies = ['050'],
+    cluster_sizes = ['005', '007', '010', '020', '050'],
+    output_file_name = None
+):
+
+    cov_data_dict = load_coefficient_of_variation_data(
+        parameters,
+        time_range = time_range_extraction,
+        accuracies = cov_accuracies,
+        principal_components = cov_principal_components
+    )
+
+    entropy_data_dict = load_entropy_data(
+        parameters, 
+        time_range = time_range_extraction,
+        cluster_sizes = cluster_sizes
+    )
+
+    cov_principal_components = [sub.replace('distance to wall', 'd2w') for sub in cov_principal_components]
+    cov_principal_components = [sub.replace('turning angle', 'angle') for sub in cov_principal_components]
+    cov_principal_components = [sub.replace('step length', 'step') for sub in cov_principal_components]
+
+    cov_id_features_dict = build_singular_ids_table(
+        cov_data_dict,
+        fish_keys,
+        time_constraint,
+        measure = 'cov',
+        modes = cov_principal_components,
+        cov_accuracy = cov_accuracies[0],
+    )
+
+    entropy_id_features_dict = build_singular_ids_table(
+        entropy_data_dict,
+        fish_keys,
+        time_constraint,
+        measure = 'entropy',
+        modes = cluster_sizes,
+    )
+
+    output_df = merge_cov_and_entropy_dicts_to_one_df(
+        cov_id_features_dict,
+        entropy_id_features_dict,
+        fish_keys,
+        time_constraint = time_constraint,
+        cov_modes = cov_principal_components,
+        cov_accuracy = cov_accuracies[0],
+        entropy_modes = cluster_sizes,
+        output_file_name = output_file_name
+    )
+    return output_df
 
 if __name__ == "__main__":
     parameters = set_parameters()
